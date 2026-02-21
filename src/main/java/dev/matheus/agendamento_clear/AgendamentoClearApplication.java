c6999b67f87a22bc67ec83aa569f15077e749566
Matheus <rmatheusluiz@gmail.com>
2026-02-21 15:52:35 -0300

    first commit

---
+++ src/main/java/dev/matheus/agendamento_clear/AgendamentoClearApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus.agendamento_clear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoClearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoClearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheus/agendamento_clear/AgendamentoClearApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheus.agendamento_clear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoClearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
