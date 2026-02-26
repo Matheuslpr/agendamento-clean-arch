2b0e7192be105567a40d3392dbab0d49e80f011c
Matheus <rmatheusluiz@gmail.com>
2026-02-25 22:18:38 -0300

    feat: adiciona documentacao swagger

--- src/main/java/dev/matheus/agendamento_clear/Infrastructure/Presentation/AgendamentoController.java
+++ src/main/java/dev/matheus/agendamento_clear/Infrastructure/Presentation/AgendamentoController.java
@@ -7,7 +7,11 @@ import dev.matheus.agendamento_clear.Infrastructure.DTO.AgendamentoResponse;
 import dev.matheus.agendamento_clear.Infrastructure.DTO.AgendamentoUpdateRequest;
 import dev.matheus.agendamento_clear.Infrastructure.Mapper.AgendamentoCreateMapper;
 import dev.matheus.agendamento_clear.Infrastructure.Mapper.AgendamentoResponseMapper;
-import dev.matheus.agendamento_clear.Infrastructure.Mapper.AgendamentoUpdadeRequestMapper;
+import dev.matheus.agendamento_clear.Infrastructure.Mapper.AgendamentoUpdateRequestMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/agendamentos")
+@Tag(name = "Agendamentos", description = "Endpoints para gerenciamento de agendamentos")
 public class AgendamentoController {
 
     private final CriarAgendamentoUseCase criarAgendamentoUseCase;
@@ -25,9 +30,9 @@ public class AgendamentoController {
     private final ConcluirAgendamentoUseCase concluirAgendamentoUseCase;
     private final AgendamentoCreateMapper agendamentoCreateMapper;
     private final AgendamentoResponseMapper agendamentoResponseMapper;
-    private final AgendamentoUpdadeRequestMapper agendamentoUpdadeRequestMapper;
+    private final AgendamentoUpdateRequestMapper agendamentoUpdadeRequestMapper;
 
-    public AgendamentoController(CriarAgendamentoUseCase criarAgendamentoUseCase, BuscarAgendamentoIdUseCase buscarAgendamentoIdUseCase, AtualizarAgendamentoUseCase atualizarAgendamentoUseCase, CancelarAgendamentoUseCase cancelarAgendamentoUseCase, ConcluirAgendamentoUseCase concluirAgendamentoUseCase, AgendamentoCreateMapper agendamentoCreateMapper, AgendamentoResponseMapper agendamentoResponseMapper, AgendamentoUpdadeRequestMapper agendamentoUpdadeRequestMapper) {
+    public AgendamentoController(CriarAgendamentoUseCase criarAgendamentoUseCase, BuscarAgendamentoIdUseCase buscarAgendamentoIdUseCase, AtualizarAgendamentoUseCase atualizarAgendamentoUseCase, CancelarAgendamentoUseCase cancelarAgendamentoUseCase, ConcluirAgendamentoUseCase concluirAgendamentoUseCase, AgendamentoCreateMapper agendamentoCreateMapper, AgendamentoResponseMapper agendamentoResponseMapper, AgendamentoUpdateRequestMapper agendamentoUpdadeRequestMapper) {
         this.criarAgendamentoUseCase = criarAgendamentoUseCase;
         this.buscarAgendamentoIdUseCase = buscarAgendamentoIdUseCase;
         this.atualizarAgendamentoUseCase = atualizarAgendamentoUseCase;
@@ -39,6 +44,11 @@ public class AgendamentoController {
     }
 
     @PostMapping
+    @Operation(summary = "Criar um novo agendamento",description = "Endpoint para criar um novo agendamento com os dados fornecidos no corpo da requisição")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Agendamento cadastrado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar agendamento")
+    })
     public ResponseEntity<Map<String, Object>> criarAgendamento(@RequestBody AgendamentoCreateRequest request){
         Agendamento criado = criarAgendamentoUseCase.execute(agendamentoCreateMapper.toEntity(request));
 
@@ -51,13 +61,25 @@ public class AgendamentoController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<AgendamentoResponse> buscarId(@PathVariable Long id){
+    @Operation(summary = "Buscar um agendamento por ID", description = "Endpoint para buscar um agendamento específico usando seu ID ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento encontrado"),
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao retorna agendamento")
+    })
+    public ResponseEntity<AgendamentoResponse> buscarId(@PathVariable("id") Long id){
         return ResponseEntity.ok(agendamentoResponseMapper.toDto(buscarAgendamentoIdUseCase.execute(id)));
     }
 
 
     @PutMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> atualizarAgendamento(@PathVariable Long id, @RequestBody AgendamentoUpdateRequest request){
+    @Operation(summary = "Atualizar um agendamento existente", description = "Endpoint para atualizar um agendamento existente usando seu ID e os dados fornecidos no corpo da requisição")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento atualizado"),
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao atualizar agendamento")
+    })
+    public ResponseEntity<Map<String, Object>> atualizarAgendamento(@PathVariable("id") Long id, @RequestBody AgendamentoUpdateRequest request){
         Agendamento existente = buscarAgendamentoIdUseCase.execute(id);
         if(existente == null){
             return ResponseEntity.notFound().build();
@@ -72,28 +94,40 @@ public class AgendamentoController {
 
 
     @PutMapping("/{id}/cancelar")
-    public  ResponseEntity<Map<String, Object>> cancelarAgendamento(@PathVariable Long id){
+    @Operation(summary = "Cancelar agendamento", description = "Endpoint para cancelar um agendamento existente usando seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento cancelado"),
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao cancelar agendamento")
+    })
+    public  ResponseEntity<Map<String, Object>> cancelarAgendamento(@PathVariable("id") Long id){
         Agendamento existente = buscarAgendamentoIdUseCase.execute(id);
         if(existente == null){
             return ResponseEntity.notFound().build();
         }
         Agendamento cancelado = cancelarAgendamentoUseCase.execute(id);
         Map<String, Object> response = new HashMap<>();
-        response.put("mensagem", "Agendamento com sucesso");
+        response.put("mensagem", "Agendamento cancelado com sucesso");
         response.put("agendamento", agendamentoResponseMapper.toDto(cancelado));
         return ResponseEntity.ok(response);
     }
 
 
     @PutMapping("/{id}/concluir")
-    public  ResponseEntity<Map<String, Object>> concluirAgendamento(@PathVariable Long id) {
+    @Operation(summary = "Concluir agendamento", description = "Endpoint para concluir um agendamento existente usando seu ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Agendamento concluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Agendamento não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Erro ao concluir agendamento")
+    })
+    public  ResponseEntity<Map<String, Object>> concluirAgendamento(@PathVariable("id") Long id) {
         Agendamento existente = buscarAgendamentoIdUseCase.execute(id);
         if (existente == null) {
             return ResponseEntity.notFound().build();
         }
         Agendamento concluir = concluirAgendamentoUseCase.execute(id);
         Map<String, Object> response = new HashMap<>();
-        response.put("mensagem", "Agendamento com sucesso");
+        response.put("mensagem", "Agendamento concluido com sucesso");
         response.put("agendamento", agendamentoResponseMapper.toDto(concluir));
         return ResponseEntity.ok(response);
 
